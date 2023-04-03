3f1d4a0542efca9bf9f26458598e50bbbac9e6bf
Chris Young <ctopheryoung@gmail.com>
2023-04-03 13:17:56 -0700

    Display game grid component

---
+++ src/components/Grid.tsx
@@ -0,0 +1,19 @@
+import "./Grid.css";
+
+export default function Board() {
+  const rowCount = 6;
+  const columnCount = 7;
+  let circleCount = 0;
+
+  return [...Array(rowCount)].map((e, rowIndex) => {
+    return (
+      <div key={rowIndex} className="grid-row">
+        {[...Array(columnCount)].map((e, columnIndex) => (
+          <span key={columnIndex} className="grid-circle">
+            {++circleCount}
+          </span>
+        ))}
+      </div>
+    );
+  });
+}
