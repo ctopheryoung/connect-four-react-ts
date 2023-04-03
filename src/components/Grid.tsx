c25c462b224e0b4c811d2618ba328af3ec687055
Chris Young <ctopheryoung@gmail.com>
2023-04-03 13:23:28 -0700

    Fix Grid jsx output

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -5,15 +5,17 @@ export default function Board() {
   const columnCount = 7;
   let circleCount = 0;
 
-  return [...Array(rowCount)].map((e, rowIndex) => {
-    return (
-      <div key={rowIndex} className="grid-row">
-        {[...Array(columnCount)].map((e, columnIndex) => (
-          <span key={columnIndex} className="grid-circle">
-            {++circleCount}
-          </span>
-        ))}
-      </div>
-    );
-  });
+  return (
+    <>
+      {[...Array(rowCount)].map((e, rowIndex) => (
+        <div key={rowIndex} className="grid-row">
+          {[...Array(columnCount)].map((e, columnIndex) => (
+            <span key={columnIndex} className="grid-circle">
+              {++circleCount}
+            </span>
+          ))}
+        </div>
+      ))}
+    </>
+  );
 }
