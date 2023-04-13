37a8d866638ded4df472b6bdccb1df971c173941
Chris Young <ctopheryoung@gmail.com>
2023-04-13 13:12:20 -0700

    Track which player's turn it is

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -5,32 +5,41 @@ import "./Grid.css";
 export default function Board() {
   const columns = 8;
   const rows = 6;
+
   const [circles, setCircles] = useState(
     Array.from({ length: columns }, (e) => Array(rows).fill(null))
   );
+  const [redIsNext, setRedIsNext] = useState(true);
 
   function handleColumnClick(columnIndex: number) {
     const nextCircles = [...circles];
     const lastOpenCircleIndex = nextCircles[columnIndex].findLastIndex(
-      (value: string | null) => !value
+      (value) => !value
     );
-    nextCircles[columnIndex][lastOpenCircleIndex] = "X";
+    nextCircles[columnIndex][lastOpenCircleIndex] = redIsNext
+      ? "red"
+      : "yellow";
     setCircles(nextCircles);
+    setRedIsNext(!redIsNext);
   }
 
   return (
-    <div className="grid">
-      {circles.map((column, columnIndex) => (
-        <div
-          key={columnIndex}
-          className="grid-column"
-          onClick={() => handleColumnClick(columnIndex)}
-        >
-          {column.map((value: string | null, i: number) => (
-            <Circle key={i} value={value} />
-          ))}
-        </div>
-      ))}
-    </div>
+    <>
+      <h2>Connect Four</h2>
+      <h4>{redIsNext ? "Red" : "Yellow"} is next.</h4>
+      <div className="grid">
+        {circles.map((column, columnIndex) => (
+          <div
+            key={columnIndex}
+            className="grid-column"
+            onClick={() => handleColumnClick(columnIndex)}
+          >
+            {column.map((value: string | null, i: number) => (
+              <Circle key={i} value={value} />
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
